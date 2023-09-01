8973e5fb6905991c9e8350ffdee412ae43d14116
Somya <isomya13@gmail.com>
2023-09-01 13:56:26 +0530

    contract deployed, send function working

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,32 +1,55 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
-// will compile your contracts, add the Hardhat Runtime Environment's members to the
-// global scope, and execute the script.
 const hre = require("hardhat");
 
+
+async function getBalances(address){
+  const balanceBigInt= await hre.ethers.provider.getBalance(address);
+  return hre.ethers.utils.formatEther(balanceBigInt);
+ }
+
+ async function consoleBalances(addresses){
+   let ct=0;
+   for(const address of addresses){
+    console.log(`Adresses ${ct} balance:`, await getBalances(address));
+    ct++;
+   }
+ }
+
+ async function consoleMemos(memos){
+  for(const memo of memos){
+    const timestamp = memo.timestamp;
+    const name = memo.name;
+    const from = memo.address;
+    const message = memo.message;
+
+    console.log("[", name, timestamp, from, message, "]");
+  }
+ }
+
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
+  const [owner, from1, from2, from3] = await hre.ethers.getSigners();
+  const defi = await hre.ethers.getContractFactory("defi");
+  const contract = await defi.deploy();
+  // console.log(contract);
+  
+  contract.deployed();
+  console.log(contract.address);
 
-  const lockedAmount = hre.ethers.parseEther("0.001");
+  const addresses = [owner.address, from1.address, from2.address, from3.address];
 
-  const lock = await hre.ethers.deployContract("Lock", [unlockTime], {
-    value: lockedAmount,
-  });
+  console.log("before transaction");
+  consoleBalances(addresses);
 
-  await lock.waitForDeployment();
-
-  console.log(
-    `Lock with ${ethers.formatEther(
-      lockedAmount
-    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.target}`
-  );
+  const amount = {value: hre.ethers.utils.parseEther("1")};
+  await contract.connect(from1).send("test1", "test1", amount);
+  await contract.connect(from2).send("test2", "test2", amount);
+  await contract.connect(from3).send("test3", "test3", amount);
+  consoleBalances(addresses);
+  console.log("after transaction");
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
+
+ 
+
 main().catch((error) => {
   console.error(error);
   process.exitCode = 1;
